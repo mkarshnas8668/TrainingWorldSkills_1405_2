12fbd059b49aad012cc63661a61c76153302d53d
Mohammad karshnas <mkarshnas6@gmail.com>
2026-05-15 16:44:42 +0330

    added 3d animationz || rotation z , rotation y , rotation x in graphicsLayer

--- Learning_Test/app/src/main/java/com/mkarshnas6/karenstudio/worldskill/ui/screen/animation/AnimationScreen.kt
+++ Learning_Test/app/src/main/java/com/mkarshnas6/karenstudio/worldskill/ui/screen/animation/AnimationScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -24,13 +25,17 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -38,18 +43,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.mkarshnas6.karenstudio.worldskill.R
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -86,6 +97,7 @@ fun AnimationScreen(
         modifier = Modifier
             .padding(20.dp)
             .background(Color.White)
+            .verticalScroll(rememberScrollState())
             .fillMaxSize(),
     ) {
         // ball move and back and change color
@@ -233,6 +245,112 @@ fun AnimationScreen(
                 modifier = Modifier.scale(scaleBall)
             )
 
+        }
+
+        // flip card
+        var flipped by remember { mutableStateOf(false) }
+        val animationRotationY by animateFloatAsState(
+            targetValue = if (flipped) 180f else 0f,
+            animationSpec = tween(1000, easing = LinearEasing)
+        )
+
+        var animationTextCard by remember { mutableStateOf("on Cart") }
+
+        LaunchedEffect(flipped) {
+            if (flipped) {
+                delay(500)
+                animationTextCard = "behind Cart"
+            } else {
+                delay(500)
+                animationTextCard = "on Cart"
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(top = 50.dp)
+                .fillMaxWidth()
+                .height(200.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(height = 240.dp, width = 160.dp)
+                    .clip(RoundedCornerShape(14.dp))
+                    .graphicsLayer {
+                        rotationY = animationRotationY
+                        cameraDistance = 100f // more deep and bather rotation
+                    }
+                    .background(Color.Blue, shape = RoundedCornerShape(14.dp))
+                    .clickable { flipped = !flipped },
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = animationTextCard,
+                    fontSize = 22.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .graphicsLayer {
+                            rotationY = animationRotationY
+                        }
+
+                )
+            }
+        }
+
+        // 3D button when click
+        var pressed by remember { mutableStateOf(false) }
+
+        val buttonRotationX by animateFloatAsState(
+            targetValue = if (pressed) 45f else 0f,
+            animationSpec = tween(200)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .graphicsLayer {
+                    rotationX = buttonRotationX
+                    cameraDistance = 10f
+                }
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Red, RoundedCornerShape(16.dp))
+                .clickable { pressed = !pressed },
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Click me !!", color = Color.White, fontSize = 24.sp)
+        }
+
+        var chartRotationY by remember { mutableStateOf(0f) }
+
+        LaunchedEffect(Unit) {
+            while (true) {
+                chartRotationY += 0.5f
+                withFrameNanos { }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    rotationY = chartRotationY
+                    cameraDistance = 20f * density
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Canvas(modifier = Modifier.size(300.dp)) {
+                val barWidth = 40f
+                val heights = listOf(200f, 350f, 500f, 280f, 420f)
+
+                heights.forEachIndexed { index, height ->
+                    drawRect(
+                        color = Color.Blue,
+                        topLeft = Offset(index * (barWidth + 20f) + 20f, 600f - height),
+                        size = Size(barWidth, height)
+                    )
+                }
+            }
         }
 
     }
